5c805a5fde7966e13ed7e32d7024b82d5414caec
j <741358192@qq.com>
2025-06-23 14:38:20 +0800

    buildscript: 更新 NuGet 包推送脚本

--- build_push.bat
+++ build_push.bat
@@ -1,4 +1,6 @@
+dotnet clean --configuration Release
 dotnet build --configuration Release
 dotnet pack --configuration Release
-dotnet nuget push "Facepunch.Steamworks/bin/Release/Facepunch.Steamworks.2.4.1.nupkg" --source "local"
-dotnet nuget push "Facepunch.Steamworks/bin/Release/Facepunch.Steamworks.2.4.1.nupkg" --source "github"
+dotnet nuget delete Facepunch.Steamworks 2.4.1 --non-interactive --source "local"
+dotnet nuget push "Facepunch.Steamworks\bin\Release\Facepunch.Steamworks.*.nupkg" --source "local"
+@REM dotnet nuget push "Facepunch.Steamworks\bin\Release\Facepunch.Steamworks.*.nupkg" --source "github"
